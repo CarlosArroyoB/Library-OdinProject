70c0640d31bc05a2624299d57813af7fb58f8f08
Carlos Arroyo Becerra <139660927+CarlosArroyoB@users.noreply.github.com>
2025-03-24 11:47:12 -0500

    adding js code

---
+++ script.js
@@ -0,0 +1,84 @@
+const myLibrary = []
+
+function Book (titulo, autor, paginas, leido){
+    this.id = crypto.randomUUID();
+    this.titulo = titulo;
+    this.autor = autor;
+    this.paginas = paginas;
+    this.leido = leido;
+}
+
+Book.prototype.toggleRead = function (){
+    this.leido = !this.leido;
+}
+function addBookToLibrary(titulo,autor,paginas,leido){
+    const newBook = new Book(titulo,autor,paginas,leido);
+    myLibrary.push(newBook);
+    renderBooks();
+}
+
+function renderBooks (){
+    const container = document.getElementById('libreria')
+    container.innerHTML = '';
+
+    libreria.forEach(libro =>{
+        const card = document.createElement("div");
+        card.classList.add('card'); 
+        card.setAttribute('data-id', libro.id);
+
+        card.innerHTML = `
+        <h2>${libro.titulo}</h2>
+        <p><strong>Autor: </strong> ${libro.autor}</p>
+        <p><strong>Paginas: </strong> ${libro.paginas}</p>
+        <p> <strong>Estado: </strong> ${libro.leido ? 'Leído' : 'No leído'}</p>
+        <button class="leidoBtn">Cambiar estado</button>
+        <button class="eliminarBtn">Eliminar</button> 
+        `;
+        container.appendChild(card);
+
+        card.querySelector('leidoBtn').addEventListener('click', () => {
+            libro.toggleRead();
+            renderBooks();
+        });
+
+        card.querySelector('eliminarBtn').addEventListener('click', () => {
+            libro.deleteBook();
+            renderBooks();  
+        });
+    })
+    
+}
+
+function deleteBook(id){
+    const index = myLibrary.findIndex(libro => libro.id === id);
+    myLibrary.splice(index, 1);
+}
+
+//modal de formulario
+const modal = document.getElementById("modal");
+const cerrarModal = document.getElementById("cerrarModal");
+const nuevoLibroBtn = document.getElementById("nuevoLibro");
+const formulario = document.getElementById("formulario");
+
+
+nuevoLibroBtn.addEventListener("click", () => {
+    modal.showModal(); // Abre el modal
+    
+});
+
+cerrarModal.addEventListener("click", () => {
+    modal.close(); // Cierra el modal
+});
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titulo = document.getElementById("titulo").value;
+    const autor = document.getElementById("autor").value;
+    const paginas = document.getElementById("paginas").value;
+    const leido = document.getElementById("leido").checked;
+
+    addBookToLibrary(titulo,autor,paginas,leido);
+    this.reset();
+    modal.close();
+
+})

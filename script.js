6ced08218e27219e7aab23e5b6bbf335864e9a18
CarloA <carlosab3007@gmail.com>
2025-04-03 14:42:07 -0500

    cambio por clases

--- script.js
+++ script.js
@@ -33,27 +33,27 @@ class Library {
         container.innerHTML = '';
 
         this.books.forEach(book => {
-            const card = document.createElement("div");
-            card.classList.add('card');
-            card.setAttribute('data-id', book.id);
+        const card = document.createElement("div");
+        card.classList.add('card');
+        card.setAttribute('data-id', book.id);
 
-            card.innerHTML = `
-                <h2>${book.titulo}</h2>
-                <p><strong>Autor: </strong> ${book.autor}</p>
-                <p><strong>Páginas: </strong> ${book.paginas}</p>
+        card.innerHTML = `
+            <h2>${book.titulo}</h2>
+            <p><strong>Autor: </strong> ${book.autor}</p>
+            <p><strong>Páginas: </strong> ${book.paginas}</p>
                 <p> <strong>Estado: </strong> ${book.leido ? 'Leído' : 'No leído'}</p>
-                <button class="leidoBtn">Cambiar estado</button>
-                <button class="eliminarBtn">Eliminar</button>
-            `;
+            <button class="leidoBtn">Cambiar estado</button>
+            <button class="eliminarBtn">Eliminar</button>
+        `;
 
             container.appendChild(card);
 
-            card.querySelector('.leidoBtn').addEventListener('click', () => {
+        card.querySelector('.leidoBtn').addEventListener('click', () => {
                 book.toggleRead();
                 this.renderBooks();
-            });
+        });
 
-            card.querySelector('.eliminarBtn').addEventListener('click', () => {
+        card.querySelector('.eliminarBtn').addEventListener('click', () => {
                 this.deleteBook(book.id);
             });
         });
@@ -68,16 +68,16 @@ const modal = document.getElementById("modal");
 const cerrarModal = document.getElementById("cerrarModal");
 const nuevoLibroBtn = document.getElementById("nuevoLibro");
 const formulario = document.getElementById("formulario");
-
+        
 nuevoLibroBtn.addEventListener("click", () => modal.showModal());
 cerrarModal.addEventListener("click", () => modal.close());
 
 formulario.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const titulo = document.getElementById("titulo").value;
-    const autor = document.getElementById("autor").value;
-    const paginas = document.getElementById("paginas").value;
-    const leido = document.getElementById("leido").checked;
+        event.preventDefault();
+        const titulo = document.getElementById("titulo").value;
+        const autor = document.getElementById("autor").value;
+        const paginas = document.getElementById("paginas").value;
+        const leido = document.getElementById("leido").checked;
 
     library.addBook(titulo, autor, paginas, leido);
 
